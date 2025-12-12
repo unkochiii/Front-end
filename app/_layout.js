a93f61f1675720671f8620c70dc51daef317076d
leouazz <leouazz@gmail.com>
2025-12-12 10:31:44 +0100

    tralala

---
+++ app/_layout.js
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import { AuthProvider } from './context/AuthContext';
+
+export default function RootLayout() {
+    return (
+        <AuthProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(auth)" />
+                <Stack.Screen name="(tabs)" />
+            </Stack>
+        </AuthProvider>
+    );
+}
